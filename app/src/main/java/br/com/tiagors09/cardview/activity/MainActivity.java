90de651787ce529009dd481d63a8197a99f76c8c
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2023-09-26 10:39:14 -0300

    adicionado interação com lista de postagens

--- app/src/main/java/br/com/tiagors09/cardview/activity/MainActivity.java
+++ app/src/main/java/br/com/tiagors09/cardview/activity/MainActivity.java
@@ -6,16 +6,24 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import br.com.tiagors09.cardview.R;
 import br.com.tiagors09.cardview.adapter.PostagemAdapter;
+import br.com.tiagors09.cardview.model.Postagem;
 
 public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerViewPostagem;
+    private List<Postagem> postagens;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.postagens = new ArrayList<Postagem>();
+        this.prepararPostagens();
 
         recyclerViewPostagem = findViewById(R.id.recyclerViewPostagens);
 
@@ -27,9 +35,30 @@ public class MainActivity extends AppCompatActivity {
         this.recyclerViewPostagem.setLayoutManager(layoutManager);
 
         // Definir adapter
-        PostagemAdapter postagemAdapter = new PostagemAdapter();
+        PostagemAdapter postagemAdapter = new PostagemAdapter(postagens);
         this.recyclerViewPostagem.setAdapter(postagemAdapter);
-
     }
 
+    public void prepararPostagens() {
+        Postagem p = new Postagem(
+                "Jamilton Damasceno",
+                "#tbt Viagem legal!",
+                R.drawable.janela_de_aviao);
+
+        this.postagens.add(p);
+
+        p = new Postagem(
+                "Miguel O'Hara",
+                "indo concertar o canon",
+                R.drawable.selfie);
+
+        this.postagens.add(p);
+
+        p = new Postagem(
+                "Paula Tejando",
+                "andando de trem",
+                R.drawable.trem);
+
+        this.postagens.add(p);
+    }
 }
